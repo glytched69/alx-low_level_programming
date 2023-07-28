3ba58042b8d4acaddf6aff180168cce7ca83ff82
youssef <youssefgaming23@gmail.com>
2023-07-27 18:47:04 -0700

    .

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,21 +1,26 @@
+; File: 101-hello_holberton.asm
+; Auth: Brennan D Baraban
+; Desc: 64-bit assembly program that prints
+;       Hello, Holberton followed by a new line.
+
 extern printf
 
 section .text
-global main
+   global main
 
 main:
-push rbp
+   push rbp
 
-mov rdi, fmt
-mov rsi, msg
-mov rax, 0
-call printf
+   mov rdi,fmt
+   mov rsi,msg
+   mov rax,0
+   call printf
 
-pop rbp
+   pop rbp
 
-mov rax, 0
-ret
+   mov rax,0
+   ret
 
 section .data
-msg: db "Hello, Holberton", 0
-fmt: db "%s", 10, 0
+   msg: db "Hello, Holberton", 0
+   fmt: db "%s", 10, 0
